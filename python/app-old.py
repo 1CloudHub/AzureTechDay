d68ba24fdb2594438d46ca38ef91168e78a62458
GeethaPandiyan <52655312+GeethaPandiyan@users.noreply.github.com>
2019-10-23 11:57:58 +0530

    Update app-old.py

--- python/app-old.py
+++ python/app-old.py
@@ -4,8 +4,10 @@ app = Flask(__name__)
 
 @app.route("/", methods=['POST', 'GET'])
 def hello():
-    return "Hello World!!"
+    hello = '<div style = "position:fixed; text-align:center; top:10px; width: 100%; height: 10px; font-size: 30px;">' \
+            '<p>Hello World!!</p></div>'
+    return hello
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=80)
+    app.run(host='0.0.0.0', port=80)
